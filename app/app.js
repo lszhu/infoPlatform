8da7a5c0cb233092a30a56a6eec9095b75004f69
Jason Zhu <jasonzhu.dev@gmail.com>
2014-11-12 20:12:57 +0800

    add district selection function

--- app/app.js
+++ app/app.js
@@ -15,8 +15,53 @@ angular.module('myApp', [
     }]);
 
 angular.module('myApp')
-    .controller('MainCtrl', ['$scope',
-        function($scope) {
+    .controller('MainCtrl', ['$scope', '$http',
+        function($scope, $http) {
+            $scope.districts = '';
+            getDistrict();
 
+            // 由服务器获取行政区划代码及对应名称
+            function getDistrict() {
+                $http.get('/district')
+                    .success(function(res) {
+                        $scope.districts = res.district;
+                        $scope.districtId = res.districtId;
+                        $scope.tmpDistrictId = res.districtId;
+                        // get parent district Id
+                        var upLevel = res.districtId.slice(0, -2);
+                        $scope.districtName = $scope
+                            .districts[upLevel][res.districtId];
+                        $scope.tmpDistrictName = $scope.districtName;
+                    }).error(function(err) {
+                        console.log('未知外界原因，无法获取行政区信息，%o', err);
+                        alert('未知外界原因，无法获取行政区信息' +
+                            JSON.stringify(err));
+                    });
+            }
+
+            // 改变列表显示的行政区域
+            $scope.changeDistrict = function(newId) {
+                if (!newId) {
+                    return;
+                }
+                var tmpId = $scope.tmpDistrictId;
+                if (newId == '..') {
+                    if (tmpId.length > 6) {
+                        $scope.tmpDistrictId = tmpId.slice(0, -2);
+                    }
+                } else if (4 < newId.length && newId.length < 10) {
+                    $scope.tmpDistrictId = newId;
+                }
+                tmpId = $scope.tmpDistrictId.slice(0, -2);
+                $scope.tmpDistrictName = $scope
+                    .districts[tmpId][$scope.tmpDistrictId];
+            };
+
+            // 设置当前行政区域
+            $scope.setDistrict = function(districtId) {
+                $scope.districtId = districtId;
+                var tmpId = districtId.slice(0, -2);
+                $scope.districtName = $scope.districts[tmpId][districtId];
+            };
         }
     ]);
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,15 @@ var auth = require('../lib/auth');
 var db = require('../lib/mongodb');
 // miscellaneous tools
 var tool = require('../lib/tool');
+// get district name and id
+var district = require('../lib/districtId');
+// set default districtId;
+var districtId = '431103';
+
+/* get district info */
+router.get('/district', function(req, res) {
+  res.send({status: 'ok', district: district, districtId: districtId});
+});
 
 /* GET home page. */
 router.get('/test', function(req, res) {
